857f88b3b94e0dbad42263480db5197b59cd328a
cacicmia <cacic.miaa@gmail.com>
2020-07-11 20:59:02 +0200

    navigation

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,21 @@
 $(document).ready(function() {
 console.log('works')
+const menuOpen = $('#menu-open')
+const menuClose = $('#menu-close')
+const pageNavigation= $('#page-navigation')
+const body = $(document.body)
+
+menuOpen.on('click', function() {
+    pageNavigation.addClass('page-nav--opened');
+    body.addClass('overflow-hidden')
+} )
+menuClose.on('click', function() {
+    pageNavigation.removeClass('page-nav--opened');
+    body.removeClass('overflow-hidden')
+})
+$(document).on('click', function(e){
+    // if (pageNavigation.hasClass('page-nav--opened') && (!pageNavigation.is(e.target) || !pageNavigation.has(e.taget))){
+    //     pageNavigation.removeClass('page-nav--opened')
+    // }   
+})
 })

12e046af2708f4317031cf338f8c78aad624fc80
Mia Ćaćić <cacic.miaa@gmail.com>
2020-07-12 15:56:44 +0200

    Footer, contact form

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,8 @@ const menuOpen = $('#menu-open')
 const menuClose = $('#menu-close')
 const pageNavigation= $('#page-navigation')
 const body = $(document.body)
+const formElements = $('.contact__form-element')
+const submitForm = $('#submit-form') 
 
 menuOpen.on('click', function() {
     pageNavigation.addClass('page-nav--opened');
@@ -13,6 +15,22 @@ menuClose.on('click', function() {
     pageNavigation.removeClass('page-nav--opened');
     body.removeClass('overflow-hidden')
 })
+submitForm.on('click', function(e) {
+    
+    form = $(this).closest('form')
+    form.addClass('submitted')
+})
+// formElements.on('focusout', function() {
+//     let element = $(this)
+//     let isValid = this.checkValidity()
+//     if (!isValid){
+//         element.addClass('has-error')
+//     } else {
+//         element.removeClass('has-error')
+
+//     }
+
+// })
 $(document).on('click', function(e){
     // if (pageNavigation.hasClass('page-nav--opened') && (!pageNavigation.is(e.target) || !pageNavigation.has(e.taget))){
     //     pageNavigation.removeClass('page-nav--opened')

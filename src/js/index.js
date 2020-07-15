147c23725f33051b2d9dbd165a55c561bd7ae110
Mia Ćaćić <cacic.miaa@gmail.com>
2020-07-15 08:09:56 +0200

    hero slider, article img animation

--- src/js/index.js
+++ src/js/index.js
@@ -4,14 +4,29 @@ $(document).ready(function() {
     const menuClose = $('#menu-close')
     const pageNavigation= $('#page-navigation')
     const body = $(document.body)
-
+    const articleImages = $('.article__image')
     const submitForm = $('#submit-form') 
     const newsletterSubmit = $('#sign-up')
+    let scrollPosition = 0
+    const slider = $('.slider__content')
+    slider.each(function() {
+        $(this).slick({
+            dots: true,
+            // slidesToScroll: 4,
+            prevArrow: $('.slider__arrow--prev'),
+            nextArrow: $('.slider__arrow--next'),
+            responsive: [
+                {
+                    breakpoint: 768,
+                    settings: {
+                        prevArrow: null,
+                        nextArrow: null
+                    }
+                }
+            ]
+            
+        })
 
-    const slider = $('#hero-slider')
-    slider.slick({
-        dots: true,
-        slidesToScroll: 4,
     })
     menuOpen.on('click', function() {
         pageNavigation.addClass('page-nav--opened');
@@ -51,6 +66,38 @@ $(document).ready(function() {
         }
     } )
 
-
+    $(document).on('scroll', function(e){
+        let windowHeight = window.innerHeight;
+        if (body[0].getBoundingClientRect().top < scrollPosition ){
+            // scroll down
+            articleImages.each(function(index) {
+                if (this.getBoundingClientRect().top < windowHeight) {
+                    let $this= $(this)
+                    const {animateClass} = $this.data()
+                    if (!$this.hasClass(animateClass)){
+                        $this.addClass(animateClass)
+                        $this.addClass('animation')    
+                        setTimeout(()=>{$this.removeClass('animation')},2000)                    
+                    } 
+                }
+            })
+        } else {
+            // scroll up
+            articleImages.each(function(index) {
+                if (this.getBoundingClientRect().bottom < windowHeight) {
+                    let $this= $(this)
+                    const {animateClass} = $this.data()
+                    if (!$this.hasClass('animation')){
+                        $this.removeClass(animateClass)
+                    }
+                    
+                }
+            })
+        }
+        
+        
+        scrollPosition = body[0].getBoundingClientRect().top
+    })
+ 
 
 })
